f2d70451226d41fa38901a8266f479792b0eabeb
Alexey Zhumaev <alexey221284@mail.ru>
2021-01-28 22:54:32 +0400

    homework-2

--- my-pokemon-game/src/index.js
+++ my-pokemon-game/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//const el = React.createElement(
+ // 'h1',
+  //null,
+  //'Hello World, React.js!'
+//);
+
+const AppList = () => {
+  return (
+        <ul>
+              <li>1</li>
+              <li>2</li>
+            </ul>
+          );
+}
+
+const AppHeader = () => {
+  return <h1>This is my Header!</h1>
+}
+
+const App = () => {
+  return (
+    <>
+    <AppHeader />
+    <AppList />
+    </>
+  )
+}
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
